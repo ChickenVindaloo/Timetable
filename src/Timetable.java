66307e20a043568526a708bd4aa22490e6cd908f
Gwen Karsten <chukya97@gmail.com>
2023-03-24 11:26:20 +0100

    First runnable version

--- src/Timetable.java
+++ src/Timetable.java
@@ -1,10 +1,33 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Timetable {
+
+
+public class Timetable extends JFrame {
     private JTextField nameInput;
     private JSpinner hours;
     private JSpinner minutes;
-    private JTextPane messageBox;
     private JButton addBtn;
     private JTextArea participantList;
+    private JTextArea messageLog;
+    private JPanel mainPanel;
+
+    public Timetable() {
+    addBtn.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            participantList.append(nameInput.getText() + hours + ':' + minutes);
+        }
+    });
+}
+
+    public static void main(String[]args){
+        Timetable timetable = new Timetable();
+        timetable.setContentPane(timetable.mainPanel);
+        timetable.setTitle("Appointment planner");
+        timetable.setSize(500, 1000);
+        timetable.setVisible(true);
+        timetable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
