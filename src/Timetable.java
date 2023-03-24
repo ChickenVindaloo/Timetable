2663db0847b54973604ab1129ef9441286e00c25
Gwen Karsten <chukya97@gmail.com>
2023-03-24 10:24:06 +0100

    Changed object names

--- src/Timetable.java
+++ src/Timetable.java
@@ -1,10 +1,10 @@
 import javax.swing.*;
 
 public class Timetable {
-    private JTextField enterANameHereTextField;
-    private JSpinner spinner1;
-    private JSpinner spinner2;
-    private JTextPane textPane1;
-    private JButton addParticipantButton;
-    private JTextArea textArea1;
+    private JTextField nameInput;
+    private JSpinner hours;
+    private JSpinner minutes;
+    private JTextPane messageBox;
+    private JButton addBtn;
+    private JTextArea participantList;
 }
