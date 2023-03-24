94cd470bcc1b04ef6b0ac62e2c03340e68285a70
Gwen Karsten <chukya97@gmail.com>
2023-03-24 12:00:54 +0100

    Can add participants

--- src/Timetable.java
+++ src/Timetable.java
@@ -17,7 +17,13 @@ public class Timetable extends JFrame {
     addBtn.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent actionEvent) {
-            participantList.append(nameInput.getText() + hours + ':' + minutes);
+            switch (nameInput.getText()) {
+                case " ", "", "enter a name here" -> messageLog.append("No valid name was entered \n");
+                default -> {
+                    participantList.append(nameInput.getText() + " at " + hours.getValue() + ':' + minutes.getValue());
+                    messageLog.append("The participant was added \n");
+                }
+            }
         }
     });
 }
@@ -26,7 +32,7 @@ public class Timetable extends JFrame {
         Timetable timetable = new Timetable();
         timetable.setContentPane(timetable.mainPanel);
         timetable.setTitle("Appointment planner");
-        timetable.setSize(500, 1000);
+        timetable.setSize(500, 800);
         timetable.setVisible(true);
         timetable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
