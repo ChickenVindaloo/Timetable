0981b529ffc73414d56888d544a9956ccc1c58a5
Gwen Karsten <chukya97@gmail.com>
2023-03-24 10:11:42 +0100

    Added interface

--- src/Timetable.java
+++ src/Timetable.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Timetable {
+import javax.swing.*;
+
+public class Timetable {
+    private JTextField enterANameHereTextField;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+    private JTextPane textPane1;
+    private JButton addParticipantButton;
+    private JTextArea textArea1;
 }
