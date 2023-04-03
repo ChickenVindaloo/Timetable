0de4949e10585d80f1e42979c425bc552616174a
Gwen Karsten <chukya97@gmail.com>
2023-04-03 13:28:13 +0200

    Minutes display properly in text message

--- src/Timetable.java
+++ src/Timetable.java
@@ -8,7 +8,7 @@ public class Timetable extends JFrame {
     private JTextField nameInput;
     SpinnerNumberModel hourModel = new SpinnerNumberModel(12, 0, 23, 1);
     private JSpinner hoursSpinner;
-    SpinnerNumberModel minuteModel = new SpinnerNumberModel(1, 0, 59, 1);
+    SpinnerNumberModel minuteModel = new SpinnerNumberModel(0, 0, 59, 1);
     private JSpinner minutesSpinner;
     private JButton addBtn;
     private JTextArea participantList;
@@ -24,8 +24,14 @@ public class Timetable extends JFrame {
             switch (nameInput.getText()) {
                 case " ", "", "enter a name here" -> messageLog.append("No valid name was entered \n");
                 default -> {
-                    participantList.append(nameInput.getText() + " at " + hoursSpinner.getValue() + ':' + minutesSpinner.getValue() + "\n");
-                    messageLog.append("The participant was added \n");
+                    int minutes = (int) minutesSpinner.getValue();
+                    if (minutes < 10) {
+                        participantList.append(nameInput.getText() + " at " + hoursSpinner.getValue() + ":0" + minutesSpinner.getValue() + "\n");
+                        messageLog.append("The participant was added \n");
+                    } else {
+                        participantList.append(nameInput.getText() + " at " + hoursSpinner.getValue() + ':' + minutesSpinner.getValue() + "\n");
+                        messageLog.append("The participant was added \n");
+                    }
                 }
             }
         }
@@ -36,7 +42,7 @@ public class Timetable extends JFrame {
         Timetable timetable = new Timetable();
         timetable.setContentPane(timetable.mainPanel);
         timetable.setTitle("Appointment planner");
-        timetable.setSize(500, 800);
+        timetable.setSize(500, 500);
         timetable.setVisible(true);
         timetable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
